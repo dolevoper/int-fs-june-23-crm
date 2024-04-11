2a5ad7e46bd0fc9ef7364aaa808311f55d4a1a27
yizhak148 <yizhak208@gmail.com>
2024-04-11 18:05:14 +0300

    commit

--- app/src/supportticket.tsx
+++ app/src/supportticket.tsx
@@ -1,13 +1,19 @@
 type Ticket = {
-    ticketID: string;
-    customerInfo:{
-      Name: string,
-      id: string  
-    };
-    createdAt: string;
-    description: string;
-    assignedUser: string;
-    status: "New" | "in progress"| "Closed";
+  ticketID: string;
+  customerInfo: {
+    Name: string;
+    id: string;
+  };
+  createdAt: string;
+  description: string;
+  assignedUser: string;
+  status: "New" | "in progress" | "Closed";
+};
+
+export function SupportTicket( ){
+  return
+  <>
+  </>
 }
 
 //create backend for ticket
